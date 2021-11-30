cd6054ac0ba302cae7808523da62177d86e26540
Victor Santos <vfreitass16@icloud.com>
2021-11-30 17:08:10 -0300

    inclusao de novos status de entrega

--- backend/src/main/java/com/senac/PI4_ecommerce/model/enums/EstadoPedido.java
+++ backend/src/main/java/com/senac/PI4_ecommerce/model/enums/EstadoPedido.java
@@ -1,45 +1,43 @@
 package com.senac.PI4_ecommerce.model.enums;
 
 public enum EstadoPedido {
-	
-	AGUARDANDO_PAGAMENTO(1, "Aguardando Pagamento"),
-	EM_SEPARACAO(2, "Em Separacao"),
-	AGUARDANDO_ENVIO(3, "Aguardando Envio"),
-	ENVIADO(3, "Enviado"),
-	ENTREGUE(3, "Entregue");
 
+	AGUARDANDO_PAGAMENTO(1, "Aguardando Pagamento"), 
+	PAGAMENTO_REJEITADO(2, "Pagamento Rejeitado"),
+	PAGAMENTO_APROVADO(3, "Pagamento Aprovado"), 
+	AGUARDANDO_RETIRADA(4, "Aguardando Retirada"),
+	EM_TRANSITO(5, "Em Transito"), 
+	ENTREGUE(6, "Entregue");
 
-	
 	private int cod;
 	private String descricao;
-	
+
 	private EstadoPedido(int cod, String descricao) {
 		this.cod = cod;
 		this.descricao = descricao;
 	}
-	
+
 	public int getCod() {
 		return cod;
 	}
-	
-	public String getDescricao () {
+
+	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public static EstadoPedido toEnum(Integer cod) {
-		
+
 		if (cod == null) {
 			return null;
 		}
-		
+
 		for (EstadoPedido x : EstadoPedido.values()) {
 			if (cod.equals(x.getCod())) {
 				return x;
 			}
 		}
-		
+
 		throw new IllegalArgumentException("Id inválido: " + cod);
 	}
 
 }
-
